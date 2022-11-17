224039c3dfba31e7b27f8dee7d376937fb28e5a7
Jill-7span <113359267+Jill-7span@users.noreply.github.com>
2022-11-17 12:16:10 +0530

    cart module complete

--- carts/cartService.js
+++ carts/cartService.js
@@ -1,5 +1,5 @@
 const model = require("../models/db");
-const { where ,Op } = require('sequelize');
+const { where, Op } = require('sequelize');
 
 
 // get cart
@@ -12,22 +12,29 @@ exports.getCartAllProduct = async (id) => {
     console.log(id);
     const data = await model.cart.findAll({
         where: {
-            buyer_id : 2
-          }})
-          
+            buyer_id: 2
+        }
+    })
+
     return data;
 }
 
-// add to cart
-exports.addToCart = async (data) => {
-    return await model.cart.create(data);
-};
-// update cart
-exports.updateToCart = async(id, quantity) => {
-    return await model.cart.update({quantity},{where:{id}})
+// add and update to cart
+exports.addAndUpdateToCart = async (cartData, buyerId, productId, quantity) => {
+    const foundItem = await model.cart.findOne({ where: { buyerId, productId } });
+    if (!foundItem) {
+        return await model.cart.create(cartData);
+    } else {
+        return await model.cart.update({ quantity }, { where: { buyerId, productId } })
+    }
 };
 
 // delete cart
-exports.deleteFromCart = async (cartId) => {
-    return await model.cart.destroy({ where: { cartId } });
+exports.deleteFromCart = async (buyerId , productId) => {
+    return await model.cart.destroy({ where: {
+        [Op.and]: [
+            { buyerId },
+            { productId}
+        ]
+    } });
 };
